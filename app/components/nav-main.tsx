94b293cfc4f708af592ee882b0024d7660b40b1e
Wilson Franca <wilson.franca.92@gmail.com>
2025-09-08 11:02:58 -0700

    feat: make quick create link to redirect to new task route

--- app/components/nav-main.tsx
+++ app/components/nav-main.tsx
@@ -27,9 +27,12 @@ export function NavMain({
             <SidebarMenuButton
               tooltip="Quick Create"
               className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+              asChild
             >
-              <IconCirclePlusFilled />
-              <span>Quick Create</span>
+              <NavLink to="/tasks/new">
+                <IconCirclePlusFilled />
+                <span>Quick Create</span>
+              </NavLink>
             </SidebarMenuButton>
             <Button
               size="icon"
